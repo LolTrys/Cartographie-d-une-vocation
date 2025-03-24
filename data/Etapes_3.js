d26bd35478354be6df69e99aa51a8b5de05b9b6c
LolTrys <colinetrystram@gmail.com>
2025-03-24 19:41:20 +0100

    Add files via upload

---
+++ data/Etapes_3.js
@@ -0,0 +1,13 @@
+var json_Etapes_3 = {
+"type": "FeatureCollection",
+"name": "Etapes_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "x": 35.82975, "y": "31.947213794743003", "Qui suis-je": "5/6    Un an plus tard, je rejoins l'UNRWA pour six mois à Amman, en Jordanie. Je veux voyager mais je veux aussi être utile. \nJe porte un regard nouveau sur le conflit israélo-palestinien. \nTout est si différent de ce qu'on peut entendre en France. " }, "geometry": { "type": "Point", "coordinates": [ 35.83, 31.947 ] } },
+{ "type": "Feature", "properties": { "x": 2.32418, "y": "48.853606978706516", "Qui suis-je": "1/6   Moi, c'est Coline. J'ai 24 ans et je viens de Paris. J'aime les voyages, la géopolitique, et poser des questions \nEn somme, je veux devenir journaliste. " }, "geometry": { "type": "Point", "coordinates": [ 2.324, 48.854 ] } },
+{ "type": "Feature", "properties": { "x": 37.63311, "y": "55.76158782053632", "Qui suis-je": "2/6    En 2020, je dois partir étudier à Moscou. \nCette ville me fascine. \nJ'aurais dû, oui, si les frontières n'avaient pas fermé à cause de la Covid-19. " }, "geometry": { "type": "Point", "coordinates": [ 37.633, 55.762 ] } },
+{ "type": "Feature", "properties": { "x": 26.7241, "y": "58.37994477316655", "Qui suis-je": "3/6    Changement de plan ! Ce sera l'Estonie, Tartu, plus précisemment. \nJe découvre l'espace post-soviétique : personne n'en parle, mais ça me passionne. \nJe ferai de son étude ma majeure. " }, "geometry": { "type": "Point", "coordinates": [ 26.724, 58.38 ] } },
+{ "type": "Feature", "properties": { "x": 17.10685, "y": "48.148209643787986", "Qui suis-je": "4/6     Le semestre s'achève, mais j'ai encore tant à découvrir avant de rentrer. Alors je rentre en stop à travers l'Europe. 8 pays, le double d'inconnus rencontrés. J'ai tout immortalisé pour ne pas oublier. J'en ferai un film. " }, "geometry": { "type": "Point", "coordinates": [ 17.107, 48.148 ] } },
+{ "type": "Feature", "properties": { "x": 2.71595, "y": "46.641505541337835", "Qui suis-je": "6/6     Je m'appelle Coline, j'ai 24 ans, et je veux être journaliste. Pour transmettre, pour informer sur ce qui n'est pas dit ailleurs. Pour lutter contre des stéréotyes nocifs à certaines cultures, à certains pays. Pour lutter contre ce terreau de l'intolérance. " }, "geometry": { "type": "Point", "coordinates": [ 2.716, 46.642 ] } }
+]
+}
